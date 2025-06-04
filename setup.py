ef1b84a71bd65d5529bce3330aa73062c4960188
Caramajau <caramia0505@gmail.com>
2025-06-04 10:36:41 +0200

    Update executable name

--- setup.py
+++ setup.py
@@ -3,5 +3,5 @@ from cx_Freeze import setup, Executable  # type: ignore
 setup(
     name="Simple Auto Clicker",
     description="A simple auto clicker with a text-based UI",
-    executables=[Executable("main.py")],
+    executables=[Executable("main.py", base=None, target_name="SimpleAutoClicker")],
 )
